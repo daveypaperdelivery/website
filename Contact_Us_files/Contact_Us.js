87d02c07ca008dab2ee8af81a82861c8bc99df90
daveypaperdelivery <142463800+daveypaperdelivery@users.noreply.github.com>
2023-08-17 13:12:24 +1000

    Add files via upload

---
+++ Contact_Us_files/Contact_Us.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20091208
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contact_Us_files/Contact_UsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
